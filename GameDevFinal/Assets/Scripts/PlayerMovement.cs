4628153a3f2f339a148475aa9c51e8eef20eb3bd
bodyb <bodyb26@bolles.org>
2025-11-10 14:24:00 -0500

    Character Movement

--- GameDevFinal/Assets/Scripts/PlayerMovement.cs
+++ GameDevFinal/Assets/Scripts/PlayerMovement.cs
@@ -15,14 +15,12 @@ public class PlayerMovemnt : MonoBehaviour
     public float yRotation = 0;
     public float xRotation = 0;
     public float maxY = 60;
-    public GameObject projectilePrefab;
     public GameObject cameraObj;
-    public GameObject deadScreen;
-    public bool dead;
-    public int zombieDead = 0;
-    public Camera camera;
     public Rigidbody rb;
     public float jumpHeight = 10;
+    public bool isGround = false;
+    public float groundDistance = 1.001f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -30,27 +28,59 @@ public class PlayerMovemnt : MonoBehaviour
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        if (!dead)
+        forwardInput = Input.GetAxis("Vertical");
+        HorizontalInput = Input.GetAxis("Horizontal");
+
+        if (isGround)
         {
-            forwardInput = Input.GetAxis("Vertical");
-            HorizontalInput = Input.GetAxis("Horizontal");
-            rb.AddForce(Vector3.forward * forwardInput * Time.deltaTime * speed);
-            rb.AddForce(Vector3.right * HorizontalInput * Time.deltaTime * speed);
-            //transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
-            //transform.Translate(Vector3.right * HorizontalInput * Time.deltaTime * speed);
-            //transform.Rotate(Vector3.up * mouseX * Time.deltaTime * turnSpeed);
-            //cameraObj.transform.Rotate(Vector3.up * mouseY * Time.deltaTime * turnSpeedY);
+            rb.AddForce(transform.forward * forwardInput * speed);
+            rb.AddForce(transform.right * HorizontalInput * speed);
+        }
+        if (!isGround)
+        {
+            rb.AddForce(transform.forward * forwardInput * speed * inAirSpeed);
+            rb.AddForce(transform.right * HorizontalInput * speed);
+        }
+        //transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
+        //transform.Translate(Vector3.right * HorizontalInput * Time.deltaTime * speed);
+        //transform.Rotate(Vector3.up * mouseX * Time.deltaTime * turnSpeed);
+        //cameraObj.transform.Rotate(Vector3.up * mouseY * Time.deltaTime * turnSpeedY);
 
-            yRotation += Mathf.Clamp(-mouseY * turnSpeedY, -maxY, maxY);
-            xRotation += mouseX * turnSpeed;
-            cameraObj.transform.rotation = Quaternion.Euler(yRotation, xRotation, 0f);
-            transform.rotation = Quaternion.Euler(0f, xRotation, 0f);
+        yRotation += Mathf.Clamp(-mouseY * turnSpeedY, -maxY, maxY);
+        xRotation += mouseX * turnSpeed;
+        cameraObj.transform.rotation = Quaternion.Euler(yRotation, xRotation + 90, 0f);
+        transform.rotation = Quaternion.Euler(0f, xRotation + 90, 0f);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            rb.AddForce(transform.up * jumpHeight);
+            isGround = false;
+            
+        }
 
 
-            mouseX = Input.GetAxis("Mouse X");
-            mouseY = Input.GetAxis("Mouse Y");
+
+        /*
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, groundDistance))
+        {
+            if (hit.collider.CompareTag("Ground"))
+            {
+                Debug.Log("hit ground");
+                isGround = true;
+            }
+        }*/
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = true;
         }
     }
 }
